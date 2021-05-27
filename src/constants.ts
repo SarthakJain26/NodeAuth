9d7c2ee4381a423c8ba0d8831ac9cee546b4c8e9
SarthakJain26 <sarthak@chaosnative.com>
2021-05-28 01:29:48 +0530

    Added create and read operations
    Signed-off-by: SarthakJain26 <sarthak@chaosnative.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const __PROD__: boolean = process.env.NODE_ENV === "production";
+export const __PORT__: number = 5000;
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,55 @@
 import express, { Request, Response } from "express";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { __PROD__ } from "./constants";
+import { __PORT__, __PROD__ } from "./constants";
+import { Student } from "./models/Student";
 
 const main = async () => {
-  const connection = await createConnection({
+  await createConnection({
     type: "postgres",
     database: "authentication",
     username: "postgres",
     password: "123",
     synchronize: !__PROD__,
-    entities: [],
+    entities: [Student],
   });
   const App = express();
+
+  App.use(express.json());
 
   App.get("/", (_: Request, res: Response) => {
     res.send("Hello World");
   });
 
-  App.listen(5000, () => {
-    console.log("listening at port 5000");
+  // Finding one student
+  App.get("/student/:roll", async (req: Request, res: Response) => {
+    const student: Student | undefined = await Student.findOne({
+      roll: parseInt(req.params.roll),
+    });
+    res.status(200).json(student);
+  });
+
+  // Fetching all students
+  App.get("/students", async (_: Request, res: Response) => {
+    const students: Student[] = await Student.find();
+    res.status(200).json({ message: "Successful", students: students });
+  });
+
+  // Registering students
+  App.post("/register", async (req: Request, res: Response) => {
+    const student: Student | undefined = await Student.findOne(req.body.roll);
+    if (!student) {
+      const response = await Student.save({
+        ...req.body,
+      });
+      res.status(200).json(response);
+    } else {
+      res.status(300).json({ error: "user already exists" });
+    }
+  });
+
+  App.listen(__PORT__, () => {
+    console.log(`listening at port ${__PORT__}`);
   });
 };
 
